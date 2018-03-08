e46a6693cb64d3bda10e9d3cc2ab1399f8512be2
Isaiah Kahler <isaiahwkahler@gmail.com>
2018-03-08 01:57:51 -0500

    what is going on

---
+++ public/script.js
@@ -0,0 +1,78 @@
+/**
+ * 
+ * @param {string} selector 
+ */
+function $(selector) {      //jquery-like dollar sign query selector
+    return document.querySelector(selector);
+};
+
+let conStatus = false;
+
+let socket = new WebSocket("ws:localhost:8080/socket");
+socket.onopen = function (event) {
+
+    let functionElements = document.getElementsByClassName("function");
+
+    for (const element of functionElements) {
+        let fxName = element.classList[1];
+        element.addEventListener("click", (event) => {
+            sendData(fxName);
+        });
+    }
+
+};
+
+function sendData(data) {
+    socket.send(data);
+}
+
+socket.addEventListener('message', event => {
+    let data = JSON.parse(event.data);
+    console.log(data);
+    switch (data.type) {
+        case "status":
+            updateStatus(data);
+            break;
+            case "alert":
+            break;
+            case "info":
+            break;
+            case "nt-data":
+                console.log(data);
+            break;
+            default:
+
+    }
+});
+
+/**
+ * 
+ * @param {JSON} data 
+ */
+function updateStatus(data){
+    if(data.status == "connected"){
+        conStatus = true;
+        $('.robot-con-status').innerHTML = "Robot is connected";
+        $('.robot-con-color').style.color = "#00ff00";
+    } else if(data.status == 'disconnected'){
+        conStatus = false;
+        $('.robot-con-status').innerHTML = "Robot is not connected";
+        $('.robot-con-color').style.color = "#ff0000";
+        if(data.hasOwnProperty("error")){
+            $('.tell-user').style.display = "block";
+            tellUser("ERROR CONNECTING TO ROBOT", JSON.stringify(data.error));
+        }
+    } else if(data.status == "connecting"){
+        $('.robot-con-status').innerHTML = "Connecting to robot...";
+        $('.robot-con-color').style.color = "#eeee00";
+    }
+}
+
+function tellUser(title, body){
+    $('.tell-user-title').innerHTML = title;
+    $('.tell-user-body').innerHTML = body;
+    $('.tell-user-close').addEventListener("click", event => {
+        $('.tell-user').style.display = "none";
+    });
+}
+
---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const expressWs = require('express-ws')(app);
+const ntClient = require('wpilib-nt-client');
+const client = new ntClient.Client();
+
+
+app.use(express.static('public'));
+// app.get('/', (req, res) => {
+// });
+
+let websock;
+
+app.ws('/socket', (ws, req) => {
+    ws.on('message', (msg) => {
+        switch (msg) {
+            case "start":
+                websock.sendJSON({
+                    type: 'status',
+                    status: 'connecting'
+                });
+                start();
+                break;
+            case "stop":
+                stop();
+                break;
+            case "test":
+                let test = {
+                    type: "test",
+                    text: "bruh this is a test"
+                };
+                ws.send(JSON.stringify(test));
+                break;
+            default:
+                console.log(`unknown command ${msg}`);
+
+        }
+    });
+    websock = ws;
+    websock.sendJSON = function (json) {
+        websock.send(JSON.stringify(json));
+    }
+});
+
+app.listen(port, () => console.log(`started server on port: ${port}`));
+
+
+function start() {
+
+
+    // Connects the client to the server on team 3571's roborio
+    client.start((isConnected, err) => {
+        // Displays the error and the state of connection
+        console.log({ isConnected, err });
+        let status = "disconnected";
+        if(isConnected){
+            let status = "connected";
+        } 
+        websock.sendJSON({
+            type: 'status',
+            text: "connection status",
+            status: status,
+            error: err
+        });
+    }, 'roborio-5263-frc.local');
+
+    // Adds a listener to the client
+    client.addListener((key, val, type, id) => {
+        // console.log({ key, val, type, id });
+        websock.sendJSON({
+            type: 'client message',
+            text: `key: ${key} val: ${val} type: ${type} id: ${id}`
+        });
+    })
+    console.log('ran start function');
+
+}
+
+function stop() {
+    console.log("ran stop function");
+    websock.sendJSON({
+        type: 'status',
+        status: 'disconnected'
+    });
+    client.stop();
+
+}
